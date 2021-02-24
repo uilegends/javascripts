5ea9a9fedd53c43d7917b4c244ec4f75ef1f2723
famousscript <uilegendsteam@gmail.com>
2021-02-24 21:49:14 +0530

    sorting array by desc

--- sorting-array.js
+++ sorting-array.js
@@ -1,4 +1,5 @@
 // Number Array sorting with Arrow function and sort() method
+// if sorting asc then write a-b and for desc write b-a
 const numberArray = [3, 5, 2, 6, 9, 8];
-const sortedArray = numberArray.sort((a, b) => a - b);
+const sortedArray = numberArray.sort((a, b) => b - a);
 console.log(sortedArray);
